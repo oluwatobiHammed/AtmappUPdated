6a928427ea1a42567824115e2e96d26fbb015c30
Oluwatobi <oladipupo_tobi@yahoo.com>
2019-09-23 15:19:28 +0100

    Initial Commit

---
+++ Atmapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Atmapp
+//
+//  Created by user on 23/09/2019.
+//  Copyright © 2019 Oladipupo Oluwatobi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
